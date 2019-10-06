c0992e1f9f303f9e59585b64000d0b43dbe13d17
Nick Warwick <nicholaswarwick@icloud.com>
2019-10-06 17:42:29 -0400

    Implement slider

--- src/components/AudioCard.js
+++ src/components/AudioCard.js
@@ -1,11 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons'
 
 export default function AudioCard({ source, label }) {
+  const DEFAULT_VOLUME = 0.5
   const [audio] = useState(new Audio(source))
   const [isPlaying, setIsPlaying] = useState(false)
-  audio.loop = true
+
+  useEffect(
+    () => {
+      audio.volume = DEFAULT_VOLUME
+      audio.loop = true
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  )
 
   function toggleAudio() {
     if (isPlaying) {
@@ -20,17 +31,31 @@ export default function AudioCard({ source, label }) {
     toggleAudio()
   }
 
+  function setVolume(volumeValue) {
+    audio.volume = volumeValue
+  }
+
   return (
-    <div className='audio-card'>
-      <div>
-        <button className='btn btn-blank' onClick={e => handleClick(e)}>
-          {isPlaying ? (
-            <FontAwesomeIcon icon={faPause} />
-          ) : (
-            <FontAwesomeIcon icon={faPlay} />
-          )}
-        </button>
-      </div>
+    <div
+      className={`audio-card ${label.toLowerCase()} ${isPlaying && 'active'}`}
+    >
+      <button
+        className='btn btn-blank'
+        onClick={volumeValue => handleClick(volumeValue)}
+      >
+        {isPlaying ? (
+          <FontAwesomeIcon icon={faPause} />
+        ) : (
+          <FontAwesomeIcon icon={faPlay} />
+        )}
+      </button>
+      <Slider
+        min={0}
+        max={1}
+        step={0.1}
+        defaultValue={DEFAULT_VOLUME}
+        onChange={volumeValue => setVolume(volumeValue)}
+      />
       {label}
     </div>
   )
